09e7ae63de7aa8db34f06df13bb4d5b27c36e931
Maxwell Rios <maxwellrios@gmail.com>
2022-07-05 21:14:25 -0300

    Criação da interface de login
    Desenvolvimento da interface de login e cadastro

---
+++ lib/uteis/paleta_cores.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PaletaCores{
+
+  static const Color colorBackground = Color(0xffD9DBD5);
+  static const Color colorPrimaria = Color(0xff075E54);
+
+}
